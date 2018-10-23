b28b867e8e7c642d58477cbacd75e9037e37d8ed
arungangula <arun.gangula@babychakra.com>
2018-10-23 13:31:25 +0530

    deleting extra files

--- app/Models/OpsAssignementLog.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class OpsAssignementLog extends Model {
-
-	protected $table = "ops_service_assignments_logs";
-}
--- app/Models/SkuBulkUpload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class SkuBulkUpload extends Model {
-    const STATUS_CREATED = 'created';
-    const STATUS_UPLOADED = 'uploaded';
-}
